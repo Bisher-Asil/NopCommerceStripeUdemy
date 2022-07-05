74dc35bde87382b2c60b17f80d31d7c9d7c7fd7d
favhwdg <45210055+favhwdg@users.noreply.github.com>
2022-07-05 14:57:24 +0300

    part 7

---
+++ src/Plugins/Nop.Plugins.Payments.StripeStandard/Validators/ConfigurationValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Nop.Plugin.Payments.StripeStandard.Models;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Plugins.Payments.StripeStandard.Validators
+{
+    public class ConfigurationValidator : BaseNopValidator<ConfigurationModel>
+    {
+        public ConfigurationValidator()
+        {
+            RuleFor(model => model.Title).NotEmpty().WithMessage("Title cannot be empty");
+
+            RuleFor(model => model.TestPublishableKey).NotEmpty().WithMessage("You are using sandbox, this is required")
+                .When(model => model.UseSandbox);
+
+            RuleFor(model => model.TestSecretKey).NotEmpty().WithMessage("You are using sandbox, this is required")
+                .When(model => model.UseSandbox);
+
+            RuleFor(model => model.LivePublishableKey).NotEmpty().WithMessage("You are not using sandbox, this is required")
+                .When(model => !model.UseSandbox);
+
+            RuleFor(model => model.LiveSecretKey).NotEmpty().WithMessage("You are not using sandbox, this is required")
+                .When(model => !model.UseSandbox);
+        }
+    }
+}
